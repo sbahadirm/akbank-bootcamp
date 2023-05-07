3db5fd077fd91bc07eb7ca458af9a366a36f60e4
bahadirmemis <sadikbahadir.memis@migrosonline.com>
2023-05-07 12:25:16 +0300

    Initial commit

---
+++ src/main/java/com/bahadirmemis/akbankbootcamp/AkbankBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.bahadirmemis.akbankbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbankBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkbankBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bahadirmemis/akbankbootcamp/AkbankBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahadirmemis.akbankbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkbankBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
