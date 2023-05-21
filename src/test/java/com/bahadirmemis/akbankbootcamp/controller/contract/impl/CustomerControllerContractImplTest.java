c1fe9dd8265b944e21cc08c4ca6cc4c022d8c0c5
bahadirmemis <sadikbahadir.memis@migrosonline.com>
2023-05-21 12:21:43 +0300

    Add save test method

--- src/test/java/com/bahadirmemis/akbankbootcamp/controller/contract/impl/CustomerControllerContractImplTest.java
+++ src/test/java/com/bahadirmemis/akbankbootcamp/controller/contract/impl/CustomerControllerContractImplTest.java
@@ -2,6 +2,7 @@ package com.bahadirmemis.akbankbootcamp.controller.contract.impl;
 
 import com.bahadirmemis.akbankbootcamp.controller.contract.CustomerControllerContract;
 import com.bahadirmemis.akbankbootcamp.dto.CustomerDTO;
+import com.bahadirmemis.akbankbootcamp.dto.CustomerSaveRequest;
 import com.bahadirmemis.akbankbootcamp.entity.Customer;
 import com.bahadirmemis.akbankbootcamp.general.BusinessException;
 import com.bahadirmemis.akbankbootcamp.service.entityservice.CustomerEntityService;
@@ -17,6 +18,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author bahadirmemis
@@ -41,12 +45,12 @@ class CustomerControllerContractImplTest {
 
     String name = "bahadir";
 
-    Customer customer = Mockito.mock(Customer.class);
-    Mockito.when(customer.getName()).thenReturn(name);
+    Customer customer = mock(Customer.class);
+    when(customer.getName()).thenReturn(name);
     List<Customer> customerList = new ArrayList<>();
     customerList.add(customer);
 
-    Mockito.when(customerEntityService.findAll()).thenReturn(customerList);
+    when(customerEntityService.findAll()).thenReturn(customerList);
 
     List<CustomerDTO> customerDTOList = customerControllerContract.findAll();
 
@@ -58,13 +62,13 @@ class CustomerControllerContractImplTest {
   @Test
   void shouldFindAllWhenReturnsCustomers() {
 
-    Customer customer1 = Mockito.mock(Customer.class);
-    Customer customer2 = Mockito.mock(Customer.class);
+    Customer customer1 = mock(Customer.class);
+    Customer customer2 = mock(Customer.class);
     List<Customer> customerList = new ArrayList<>();
     customerList.add(customer1);
     customerList.add(customer2);
 
-    Mockito.when(customerEntityService.findAll()).thenReturn(customerList);
+    when(customerEntityService.findAll()).thenReturn(customerList);
 
     List<CustomerDTO> customerDTOList = customerControllerContract.findAll();
     Assertions.assertEquals(2, customerDTOList.size());
@@ -72,6 +76,23 @@ class CustomerControllerContractImplTest {
 
   @Test
   void shouldSave() {
+
+    Long id = 18L;
+    String name = "bahadir";
+
+    CustomerSaveRequest request = mock(CustomerSaveRequest.class);
+    Customer customer = mock(Customer.class);
+    when(customer.getId()).thenReturn(id);
+
+    when(request.name()).thenReturn(name);
+    when(customerEntityService.save(any())).thenReturn(customer);
+
+    CustomerDTO result = customerControllerContract.save(request);
+
+    Mockito.verify(customerEntityService).save(any());
+    assertEquals(id, result.id());
+    //assertEquals(name, result.name()); eziliyor!!!
+
   }
 
   @Test
